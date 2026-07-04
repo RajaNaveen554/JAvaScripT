8bafb71a9bd1241a008802e689453dc818c364a3
Raja Naveen <rajanaveentirumalasetti521@gmail.com>
2026-07-04 21:01:56 +0530

    Immediately Invoked Function

---
+++ Functions/IIFE/prog1.js
@@ -0,0 +1,11 @@
+//IIFE (Immediately Invoked Function Expression)
+// What is an IIFE?
+
+// An IIFE is a function that:
+
+// Is created.
+// Executes immediately.
+// Doesn't need to be called later
+(function() {
+    console.log("Hello");
+})();
---
+++ Functions/IIFE/prog4.js
@@ -0,0 +1,9 @@
+(function(){
+    let n=5;
+    let fact=1;
+    for(let i=2;i<=n;i++)
+    {
+        fact*=i;
+    }
+    console.log(fact);
+})();
